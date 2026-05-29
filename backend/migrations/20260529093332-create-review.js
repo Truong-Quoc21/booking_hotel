177053607fea6d29433d39839f888f3d4c8fc907
truongquoc <quoc93346@gmail.com>
2026-05-29 17:06:11 +0700

    Create 20260529093332-create-review.js

---
+++ backend/migrations/20260529093332-create-review.js
@@ -0,0 +1,36 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('reviews', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      user_id: {
+        type: Sequelize.INTEGER
+      },
+      hotel_id: {
+        type: Sequelize.INTEGER
+      },
+      booking_id: {
+        type: Sequelize.INTEGER
+      },
+      rating: {
+        type: Sequelize.INTEGER
+      },
+      comment: {
+        type: Sequelize.TEXT
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('reviews');
+  }
+};
