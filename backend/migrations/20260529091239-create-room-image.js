62376b844be17c5ec16975a3e663801bcc63bc7e
truongquoc <quoc93346@gmail.com>
2026-05-29 17:05:57 +0700

    Create 20260529091239-create-room-image.js

---
+++ backend/migrations/20260529091239-create-room-image.js
@@ -0,0 +1,23 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('roomImages', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      room_id: {
+        type: Sequelize.INTEGER
+      },
+      image_url: {
+        type: Sequelize.STRING
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('roomImages');
+  }
+};
