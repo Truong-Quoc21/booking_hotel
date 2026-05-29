8f1275530ca56a6d85dd17c790252859ebc7adb4
truongquoc <quoc93346@gmail.com>
2026-05-29 21:50:29 +0700

    Update review.js

--- backend/models/review.js
+++ backend/models/review.js
@@ -10,7 +10,9 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      Review.belongsTo(models.User, { foreignKey: 'user_id' });
+      Review.belongsTo(models.Hotel, { foreignKey: 'hotel_id' });
+      Review.belongsTo(models.Booking, { foreignKey: 'booking_id' });
     }
   }
   Review.init({
