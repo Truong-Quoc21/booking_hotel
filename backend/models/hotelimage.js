020fa01edee4d820b98b6403522e02a39fd93258
truongquoc <quoc93346@gmail.com>
2026-05-29 21:50:22 +0700

    Update hotelimage.js

--- backend/models/hotelimage.js
+++ backend/models/hotelimage.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      HotelImage.belongsTo(models.Hotel, { foreignKey: 'hotel_id' });
     }
   }
   HotelImage.init({
