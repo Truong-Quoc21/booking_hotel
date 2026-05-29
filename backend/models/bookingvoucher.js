331fab02482c233d39838b3ba8c38abc52409092
truongquoc <quoc93346@gmail.com>
2026-05-29 21:50:15 +0700

    Update bookingvoucher.js

--- backend/models/bookingvoucher.js
+++ backend/models/bookingvoucher.js
@@ -10,7 +10,8 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      BookingVoucher.belongsTo(models.Booking, { foreignKey: 'booking_id' });
+      BookingVoucher.belongsTo(models.Voucher, { foreignKey: 'voucher_id' });
     }
   }
   BookingVoucher.init({
