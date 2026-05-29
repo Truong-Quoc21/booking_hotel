0d9026a808a675c92a13d7e619b12e78ef221955
truongquoc <quoc93346@gmail.com>
2026-05-29 21:50:08 +0700

    Update amenity.js

--- backend/models/amenity.js
+++ backend/models/amenity.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      Amenity.belongsToMany(models.Hotel, { through: models.HotelAmenity, foreignKey: 'amenity_id' });
     }
   }
   Amenity.init({
