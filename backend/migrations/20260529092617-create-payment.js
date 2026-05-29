8b86dae67bf697914a3f0ac48b36cfcf202e5c09
truongquoc <quoc93346@gmail.com>
2026-05-29 17:06:09 +0700

    Create 20260529092617-create-payment.js

---
+++ backend/migrations/20260529092617-create-payment.js
@@ -0,0 +1,39 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('payments', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      booking_id: {
+        type: Sequelize.INTEGER
+      },
+      payment_method: {
+        type: Sequelize.STRING(50)
+      },
+      amount: {
+        type: Sequelize.DECIMAL(10, 2)
+      },
+      currency: {
+        type: Sequelize.STRING(10),
+        defaultValue: 'VND'
+      },
+      transaction_code: {
+        type: Sequelize.STRING(100)
+      },
+      payment_status: {
+        type: Sequelize.STRING(50)
+      },
+      paid_at: {
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('payments');
+  }
+};
