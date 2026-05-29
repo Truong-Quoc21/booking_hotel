a05e50fdb87ed60ab826c96dc79d7e4cf0089044
truongquoc <quoc93346@gmail.com>
2026-05-29 21:50:38 +0700

    Update voucher.js

--- backend/models/voucher.js
+++ backend/models/voucher.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      Voucher.belongsToMany(models.Booking, { through: models.BookingVoucher, foreignKey: 'voucher_id' });
     }
   }
   Voucher.init({
