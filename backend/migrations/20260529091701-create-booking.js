1d7607309f38ee42a465e0256e19f6e1dafeb56d
truongquoc <quoc93346@gmail.com>
2026-05-29 17:06:05 +0700

    Create 20260529091701-create-booking.js

---
+++ backend/migrations/20260529091701-create-booking.js
@@ -0,0 +1,48 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('bookings', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      user_id: {
+        type: Sequelize.INTEGER
+      },
+      hotel_id: {
+        type: Sequelize.INTEGER
+      },
+      check_in_date: {
+        type: Sequelize.DATEONLY
+      },
+      check_out_date: {
+        type: Sequelize.DATEONLY
+      },
+      num_guests: {
+        type: Sequelize.INTEGER
+      },
+      total_amount: {
+        type: Sequelize.DECIMAL(10, 2)
+      },
+      booking_status: {
+        type: Sequelize.STRING(50)
+      },
+      payment_status: {
+        type: Sequelize.STRING(50)
+      },
+      special_request: {
+        type: Sequelize.TEXT
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('bookings');
+  }
+};
