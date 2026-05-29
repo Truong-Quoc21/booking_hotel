1a41aebe4c549c73df02ea51b107d34ed7c3c93a
truongquoc <quoc93346@gmail.com>
2026-05-29 17:05:48 +0700

    Create 20260529085243-create-hotel.js

---
+++ backend/migrations/20260529085243-create-hotel.js
@@ -0,0 +1,66 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('hotels', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      description: {
+        type: Sequelize.TEXT
+      },
+      address: {
+        type: Sequelize.STRING
+      },
+      city: {
+        type: Sequelize.STRING(100)
+      },
+      country: {
+        type: Sequelize.STRING(100)
+      },
+      star_rating: {
+        type: Sequelize.INTEGER
+      },
+      phone: {
+        type: Sequelize.STRING(20)
+      },
+      email: {
+        type: Sequelize.STRING(100)
+      },
+      thumbnail: {
+        type: Sequelize.STRING
+      },
+      check_in_time: {
+        type: Sequelize.TIME
+      },
+      check_out_time: {
+        type: Sequelize.TIME
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      },
+      deleted_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('hotels');
+  }
+};
