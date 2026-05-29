5687eafbf8f94d99aed7513e92b6e9de2bd772d1
truongquoc <quoc93346@gmail.com>
2026-05-29 21:50:40 +0700

    Update wishlist.js

--- backend/models/wishlist.js
+++ backend/models/wishlist.js
@@ -10,7 +10,8 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      Wishlist.belongsTo(models.User, { foreignKey: 'user_id' });
+      Wishlist.belongsTo(models.Hotel, { foreignKey: 'hotel_id' });
     }
   }
   Wishlist.init({
