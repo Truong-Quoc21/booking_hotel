644c9a20820b5d326f561f1c4e1b0c9f7dc2f393
truongquoc <quoc93346@gmail.com>
2026-05-29 21:50:35 +0700

    Update roomtype.js

--- backend/models/roomtype.js
+++ backend/models/roomtype.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      RoomType.hasMany(models.Room, { foreignKey: 'room_type_id' });
     }
   }
   RoomType.init({
