b87e575ca2b931e1819feba49c36a1f08263347d
truongquoc <quoc93346@gmail.com>
2026-06-14 12:33:15 +0700

    Create imageUpload.js

---
+++ backend/middlewares/imageUpload.js
@@ -0,0 +1,28 @@
+import path from 'path'
+import multer from 'multer'
+const storage = multer.diskStorage({
+    destination: function(req, res, callback) {
+        const destinationPath = path.join(__dirname, '../uploads/')
+        callback(null, destinationPath)
+    },
+    filename: function(req, file, callback) {
+        callback(null, file.originalname)
+    },
+})
+
+const fileFilter = (req, file, callback) => {
+    if(file.mimetype.startsWith('image')){
+        callback(null, true);
+    } else {
+        callback(new Error('Chỉ được phép tải file ảnh!'), false);
+    }
+}
+
+const upload = multer({
+    storage,
+    fileFilter, 
+    limits: {
+        fileSize: 1024 * 1024 * 5
+    }
+})
+export default upload
