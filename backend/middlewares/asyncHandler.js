d466c3568de2f582e7e6b2c152f7ecbbcaaf07de
truongquoc <quoc93346@gmail.com>
2026-06-14 12:33:13 +0700

    Create asyncHandler.js

---
+++ backend/middlewares/asyncHandler.js
@@ -0,0 +1,16 @@
+const asyncHandler = (fn) => {
+    return async (req, res, next) => {
+        try {
+            await fn(req, res, next)
+        }catch(error) {
+            console.error('Detailed Error: ', error);
+            console.log('Error Details:', {message:error.message, stack:error.stack});
+            return res.status(500).json({
+                message: 'Internal Sever Error',
+                error: process.env.NODE_ENV === 'development' ? error : ''
+            })
+        }
+    }
+}
+
+export default asyncHandler;
