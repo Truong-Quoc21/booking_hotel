32c7b5a65111f59c1fcb238fe37cc9fdc93e11bc
truongquoc <quoc93346@gmail.com>
2026-06-14 12:33:10 +0700

    Create userResponese.js

---
+++ backend/dtos/responese/user/userResponese.js
@@ -0,0 +1,10 @@
+class userResponese {
+    constructor(user) {
+        this.full_name = user.full_name;
+        this.email = user.email;
+        this.phone = user.phone;
+        this.role = user.role;
+        this.status = user.status;
+    }
+}
+export default userResponese;
